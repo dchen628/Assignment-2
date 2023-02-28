3bf95107988d599dc8d6e44fa9261d939fec5746
David Chen <dav@Davids-MacBook-Pro.local>
2023-02-28 14:18:16 -0500

    myReduce added to javascript

--- javascript-methods.js
+++ javascript-methods.js
@@ -63,6 +63,27 @@ Array.prototype.myEvery = function(callbackFn) {
 // REDUCE //
 Array.prototype.myReduce = function(callbackFn) {
   // Place your code here.
+  let accumulator;
+    if(initialValue === undefined)
+    {
+      accumulator = initialValue;
+    }
+    else
+    {
+      accumulator = initialValue;
+    }
+    for(let i = 0; i < this.length; i++)
+    {
+        if(accumulator !== undefined)
+        {
+          accumulator = callbackFn.call(undefined, accumulator, this[i], i, this)
+        }
+        else
+        {
+          accumulator = this[i]
+        }
+    }
+    return accumulator;
 };
 
 // INCLUDES //
